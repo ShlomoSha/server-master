162c7f2f3a81a1b310af78e4da475365c2e83336
shlomoShalem <s0552288304@gmail.com>
2024-11-18 09:47:22 +0200

    added types and ping test

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import usersController from "./controllers/users";
@@ -6,24 +6,24 @@ import adminController from "./controllers/admin";
 import votesController from "./controllers/votes";
 import candidatesController from "./controllers/candidates";
 import { connectToMongo } from "./config/db";
-import http from "http"
-import { Server } from "socket.io"
+import http from "http";
+import { Server } from "socket.io";
 import { handleSocketConnection } from "./sockets/io";
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-const httpServer = http.createServer(app)
-export const io  = new Server(httpServer, {
+export const app = express();
+const httpServer = http.createServer(app);
+export const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: "*",
-  }
-})
+  },
+});
 
-io.on("connection", handleSocketConnection)
+io.on("connection", handleSocketConnection);
 
-connectToMongo()
+connectToMongo();
 
 app.use(express.json());
 app.use(cors());
@@ -32,6 +32,10 @@ app.use("/api/users", usersController);
 app.use("/api/admin", adminController);
 app.use("/api/votes", votesController);
 app.use("/api/candidates", candidatesController);
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("pong");
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server started, Visit "http://localhost:${PORT}"`);
